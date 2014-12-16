ad1bf446f7aed9ce7c204cbe13ff7be72a616e52
sandrasvensson <sandra.svensson@hyperisland.se>
2014-12-16 14:22:39 +0100

    grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,23 @@
 module.exports = function (grunt) {
 	grunt.initConfig({
+		
+		pkg: grunt.file.readJSON('package.json'),
+
+		// UGLIFY
 		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */',
+				beautify: false, 
+				preserveComments: false 
+			},
 			build: {
-			    src: 'js/*.js', 
-				dest: 'build/js/*.min.js'
+			    src: 'js/main.js', 
+				dest: 'build/js/main.min.js'
 			}
 		}, 
+
+
+		// CSS MINIFY 
 		cssmin: {
 		  minify: {
 		    expand: true,
